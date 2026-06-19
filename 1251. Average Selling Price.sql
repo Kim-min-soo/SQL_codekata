b75bf98643449633c3ee40ddbb19075fa1001b37
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 18:48:36 +0900

    LeetCode 중급

---
+++ 1251. Average Selling Price.sql
@@ -0,0 +1,12 @@
+-- 1251. Average Selling Price
+-- https://leetcode.com/problems/average-selling-price/description/
+SELECT
+    p.product_id,
+    IFNULL(ROUND(SUM(p.price * u.units) / SUM(u.units),2),0) AS average_price
+FROM
+    Prices p Left Join UnitsSold u
+    ON p.product_id = u.product_id
+    AND u.purchase_date >= p.start_date
+    AND u.purchase_date <= p.end_date
+GROUP BY
+    p.product_id
