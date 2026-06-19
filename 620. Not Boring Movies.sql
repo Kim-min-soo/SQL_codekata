3c70bccb3615c2f4b1b1798e37238df18d938d19
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 17:47:53 +0900

    LeetCode 입문

---
+++ 620. Not Boring Movies.sql
@@ -0,0 +1,6 @@
+-- 620. Not Boring Movies
+-- https://leetcode.com/problems/not-boring-movies/description/
+SELECT *
+FROM Cinema
+WHERE id % 2 !=0 AND description != 'boring'
+ORDER BY rating DESC;
