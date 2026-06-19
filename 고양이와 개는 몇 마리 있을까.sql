cc38c736f36834c9769f5717388bd343f3f1abe6
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 16:59:06 +0900

    프로그래머스 기초

---
+++ 고양이와 개는 몇 마리 있을까.sql
@@ -0,0 +1,13 @@
+-- 고양이와 개는 몇 마리 있을까
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59040
+SELECT
+    ANIMAL_TYPE,
+    COUNT(ANIMAL_ID)
+FROM
+    ANIMAL_INS
+WHERE
+    ANIMAL_TYPE IN('Cat', 'Dog')
+GROUP BY
+    ANIMAL_TYPE
+ORDER BY
+    ANIMAL_TYPE;
