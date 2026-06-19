d718f9ec4399ee81548d1fbf14d17a5af9073cd3
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 17:08:35 +0900

    프로그래머스 기초
    Query to select fruit-based ice creams with high orders.

---
+++ 과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,12 @@
+-- 과일로 만든 아이스크림 고르기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133025
+SELECT 
+    F.FLAVOR
+FROM
+    FIRST_HALF F INNER JOIN ICECREAM_INFO I
+    ON F.FLAVOR = I.FLAVOR
+WHERE
+    F.TOTAL_ORDER > 3000
+    AND I.INGREDIENT_TYPE = 'fruit_based'
+ORDER BY 
+    TOTAL_ORDER DESC;
