a1f68b0f771cc0ff3df74bcb319445699c71e368
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 17:18:32 +0900

    프로그래머스 중급

---
+++ 오랜 기간 보호한 동물(1).sql
@@ -0,0 +1,13 @@
+-- 오랜 기간 보호한 동물(1)
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59044
+SELECT 
+    I.NAME,
+    I.DATETIME
+FROM
+    ANIMAL_INS I LEFT JOIN ANIMAL_OUTS O
+    ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE 
+    O.ANIMAL_ID IS NULL
+ORDER BY
+    DATETIME
+LIMIT 3
