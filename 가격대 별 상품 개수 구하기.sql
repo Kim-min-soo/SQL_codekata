9e119f61d17b8937ea6562e25a3143451719038c
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 17:07:23 +0900

    프로그래머스 기초

---
+++ 가격대 별 상품 개수 구하기.sql
@@ -0,0 +1,11 @@
+-- 가격대 별 상품 개수 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131530
+SELECT
+    FLOOR(PRICE/10000)*10000 AS PRICE_GROUP,
+    COUNT(*) AS PRODUCTS
+FROM
+    PRODUCT
+GROUP BY
+    FLOOR(PRICE/10000)
+ORDER BY
+    PRICE_GROUP
