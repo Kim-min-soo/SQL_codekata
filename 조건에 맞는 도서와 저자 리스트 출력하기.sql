5ae53ff1668391d215d99828a4c84f1337b237c3
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 17:06:16 +0900

    프로그래머스 기초

---
+++ 조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,13 @@
+-- 조건에 맞는 도서와 저자 리스트 출력하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/144854
+SELECT 
+    B.BOOK_ID,
+    A.AUTHOR_NAME,
+    DATE_FORMAT(B.PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
+FROM 
+    BOOK B INNER JOIN AUTHOR A
+    ON B.AUTHOR_ID = A.AUTHOR_ID
+WHERE
+    CATEGORY = '경제'
+ORDER BY
+    B.PUBLISHED_DATE;
