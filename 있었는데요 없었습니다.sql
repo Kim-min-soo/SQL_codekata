3e91bd37547f5cad53d6892cb6ad52b4ca924459
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 17:04:18 +0900

    프로그래머스 기초

---
+++ 있었는데요 없었습니다.sql
@@ -0,0 +1,12 @@
+-- 있었는데요 없었습니다
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59043
+SELECT 
+    I.ANIMAL_ID,
+    I.NAME
+FROM 
+    ANIMAL_INS I INNER JOIN ANIMAL_OUTS O
+    ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE 
+    I.DATETIME > O.DATETIME
+ORDER BY
+    I.DATETIME
