51827a94d6d86580134162a3c68a7f44acae1bb8
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 17:45:47 +0900

    LeetCode 입문
    Add SQL query to select big countries based on area and population.

---
+++ 595. Big Countries.sql
@@ -0,0 +1,10 @@
+-- 595. Big Countries
+-- https://leetcode.com/problems/big-countries/description/
+SELECT
+    name,
+    population,
+    area
+FROM
+    World
+WHERE
+    area >= 3000000 or population >= 25000000
