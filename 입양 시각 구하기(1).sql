7f526c140e99635ecbadf0824ee255605f236f1b
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 16:59:42 +0900

    프로그래머스 기초
    Add SQL query to retrieve adoption hours and counts.

---
+++ 입양 시각 구하기(1).sql
@@ -0,0 +1,12 @@
+-- 입양 시각 구하기(1)
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59412
+SELECT
+    HOUR(DATETIME) AS HOUR,
+    COUNT(*) AS COUNT
+FROM 
+    ANIMAL_OUTS
+WHERE HOUR(DATETIME)>=9 AND HOUR(DATETIME)<20 
+GROUP BY 
+    HOUR(DATETIME)
+ORDER BY
+    HOUR
