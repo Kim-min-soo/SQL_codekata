2e3428e514ac31e5c0d90006e383d562ed821f5e
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 17:48:22 +0900

    LeetCode 입문
    Add SQL query to determine if three sides form a triangle.

---
+++ 610. Triangle Judgement.sql
@@ -0,0 +1,8 @@
+-- 610. Triangle Judgement
+-- https://leetcode.com/problems/triangle-judgement/description/
+SELECT 
+    x,
+    y,
+    z,
+    if((x<y+z and y<x+z and z<x+y), 'Yes', 'No') as triangle
+FROM Triangle
