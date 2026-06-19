54769cd78e600badd3100c81bc5bd9bd57c14920
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 17:22:54 +0900

    프로그래머스 중급

---
+++ 조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,16 @@
+-- 조건에 맞는 사용자 정보 조회하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/164670
+SELECT 
+    U.USER_ID,
+    U.NICKNAME,
+    CONCAT(U.CITY, ' ', U.STREET_ADDRESS1, ' ', U.STREET_ADDRESS2) AS '전체주소',
+    CONCAT(LEFT(U.TLNO,3), '-', SUBSTR(U.TLNO,4,4), '-', RIGHT(U.TLNO,4)) AS '전화번호'
+FROM 
+    USED_GOODS_BOARD B INNER JOIN USED_GOODS_USER U
+    ON B.WRITER_ID = U.USER_ID
+GROUP BY 
+    B.WRITER_ID
+HAVING 
+    COUNT(*)>=3
+ORDER BY
+    U.USER_ID DESC;
