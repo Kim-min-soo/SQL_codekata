fffde2e4e2cb66432184ff5eb83b649ce74c8cf0
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 17:48:57 +0900

    LeetCode 입문
    Fixes the names formatting in the Users table by capitalizing the first letter and lowercasing the rest.

---
+++ 1667. Fix Names in a Table.sql
@@ -0,0 +1,7 @@
+-- 1667. Fix Names in a Table
+-- https://leetcode.com/problems/fix-names-in-a-table/description/
+SELECT 
+    user_id,
+    concat(upper(left(name, 1)),lower(right(name,length(name)-1))) as name
+FROM Users
+order by user_id
