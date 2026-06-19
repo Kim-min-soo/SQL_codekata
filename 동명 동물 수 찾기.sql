bae70eda0693c98a9faad3a9c84ed92454082d01
Kim-min-soo <msoo5880@yu.ac.kr>
2026-06-19 16:57:46 +0900

    프로그래머스 기초

---
+++ 동명 동물 수 찾기.sql
@@ -0,0 +1,9 @@
+-- 동명 동물 수 찾기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59041
+SELECT
+    NAME,
+    COUNT(NAME) AS COUNT
+FROM ANIMAL_INS
+GROUP BY NAME
+HAVING COUNT(NAME)>=2
+ORDER BY NAME;
